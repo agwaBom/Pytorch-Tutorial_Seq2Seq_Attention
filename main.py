e35e7efee3afe9c0390da15115f506b97ba9ae2c
Kim Hyun Jin <khyunjin1993@gmail.com>
2021-03-22 18:24:32 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# It's basically the equivalent of "I guess you're not ready for this, but your kids are gonna love it". 
+from __future__ import unicode_literals, print_function, division
+
+import preprocessing
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as f
+
+
+#device = torch.device("cuda" if torch.cuda.is_avaliable() else "cpu")
+
+input, output, pairList = preprocessing.prepareData('eng', 'fra')
+print("input : ", input)
+print('output : ', output)
+print('pairlist : ', pairList)
---
+++ model.py
@@ -0,0 +1,54 @@
+from preprocessing import MAX_LENGTH
+from torch._C import device
+import torch.nn.functional as F
+import torch.nn as nn
+import torch
+
+MAX_LENGTH = 10
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+    
+    def forward(self, input, hidden):
+        # reshapes x into size (a,b,...)        
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
+        super(AttnDecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.max_length = max_length
+
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
