50138c1350a3b9b0dc4b3c1e79138172aa364fbe
agwaBom <khyunjin1993@gmail.com>
2021-03-30 14:08:09 +0900

    full_code working

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,8 @@
 import unicodedata
 import re
+import torch
+from model import device
+from main import input_lang, output_lang
 
 """
 The full process for preparing the data is:
@@ -98,3 +101,17 @@ def prepareData(lang1, lang2, reverse=False):
     print(input_lang.name, input_lang.n_words)
     print(output_lang.name, output_lang.n_words)
     return input_lang, output_lang, pairList
+
+
+def indexesFromSentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')]
+
+def tensorFromSentence(lang, sentence):
+    indexes = indexesFromSentence(lang, sentence)
+    indexes.append(EOS_token)
+    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+
+def tensorsFromPair(pair):
+    input_tensor = tensorFromSentence(input_lang, pair[0])
+    target_tensor = tensorFromSentence(output_lang, pair[1])
+    return (input_tensor, target_tensor)
